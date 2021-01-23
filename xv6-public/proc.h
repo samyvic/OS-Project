ab66209ba2874053ce757b10177124c305772403
samyvic <47760339+samyvic@users.noreply.github.com>
2021-01-23 22:34:21 +0200

    V1.1.5
    Declaring a variable to track the number of tickets a process has been running

--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int tickets; 		       // Variable for tracking the number of tickets of a process
 };
 
 // Process memory is laid out contiguously, low addresses first:
